979b634b0baf6acdca538ffa450726796bc5131d
heri <heri@studiozenkai.com>
2017-05-23 21:24:28 -0400

    - Amounts are rounded not ceiled to lowest cent (Email Ella) - Added unit test for electronics - Added unit test for flat markup

--- product.rb
+++ product.rb
@@ -3,15 +3,16 @@ class Product
 
   FLAT_MARKUP = 5
   WORKER_MARKUP = 1.2
-  CATEGORIES_MARKUP = { pharmaceuticals: 7.5, food: 13, electronics: 2, drugs: 7.5 }
+  CATEGORIES_MARKUP = { food: 13, electronics: 2, drugs: 7.5 }
 
   def self.final_cost(base_price: 0.0, workers: 0, category: '')
-    raise ArgumentError.new('Strictly positive price required') unless base_price > 0 and base_price.is_a?(Numeric)
-    raise ArgumentError.new('Finite positive number for workers') unless workers >= 0 and workers.is_a?(Integer)
-    raise ArgumentError.new('Category are strings') unless category.is_a?(String)
+    # validate arguments
+    raise(ArgumentError, 'Strictly positive price required') unless base_price > 0 && base_price.is_a?(Numeric)
+    raise(ArgumentError, 'Finite positive number for workers') unless workers >= 0 && workers.is_a?(Integer)
+    raise(ArgumentError, 'Category are strings') unless category.is_a?(String)
 
     cost = add_markup(base_price, FLAT_MARKUP)
-    additional_markup = WORKER_MARKUP * workers + (CATEGORIES_MARKUP[category.to_sym] ? CATEGORIES_MARKUP[category.to_sym] : 0)
+    additional_markup = WORKER_MARKUP * workers + (CATEGORIES_MARKUP[category.to_sym] || 0)
     cost = add_markup(cost, additional_markup) if additional_markup > 0
     round_cents(cost)
   end
@@ -23,6 +24,6 @@ class Product
   end
 
   def self.round_cents(price)
-    (price * 100).ceil / 100.0
+    (price * 100).round / 100.0
   end
 end
--- product_test.rb
+++ product_test.rb
@@ -1,19 +1,37 @@
 require_relative 'product'
 require 'test/unit'
 
+# tests final cost calculations based on based price,
+# number of people working and product category
 class TestMarkup < Test::Unit::TestCase
-  def test_simple
+  def test_base_examples
     assert_equal 1_591.58, Product.final_cost(base_price: 1_299.99, workers: 3, category: 'food')
     assert_equal 6_199.81, Product.final_cost(base_price: 5_432.00, workers: 1, category: 'drugs')
     assert_equal 13_707.63, Product.final_cost(base_price: 12_456.95, workers: 4, category: 'books')
+  end
+
+  def test_electronics
+    assert_equal 1_392.29, Product.final_cost(base_price: 1_299.99, workers: 0, category: 'electronics')
+  end
+
+  def test_only_flat
+    # this assumes no workers and no category
+    # it should only apply the 5% flat markup
+    assert_equal 1_364.99, Product.final_cost(base_price: 1_299.99)
   end
 
   def test_no_worker
     # no workers should add a 0% markup
   end
 
-  def tet_no_category
+  def test_no_category
     # other categories is a 0% markup
+  end
+
+  def test_round_cents
+    # a test to make round cents pass
+
+    # a test to make round cents fail
   end
 
   def test_edge_cases
@@ -21,13 +39,19 @@ class TestMarkup < Test::Unit::TestCase
     assert_raises ArgumentError do
       Product.final_cost(base_price: -21, workers: 3, category: 'food')
     end
+    assert_raises ArgumentError do
+      Product.final_cost(base_price: 0, workers: 3, category: 'food')
+    end
     # number of workers in product should be positive
     assert_raises ArgumentError do
-      Product.final_cost(base_price: 1, workers: -1, category: 'food')
+      Product.final_cost(base_price: 1_299.99, workers: -1, category: 'food')
+    end
+    assert_raises ArgumentError do
+      Product.final_cost(base_price: 1_299.99, workers: 1.2, category: 'food')
     end
     # category should be a string
     assert_raises ArgumentError do
-      Product.final_cost(base_price: 1, workers: 1, category: 1)
+      Product.final_cost(base_price: 1_299.99, workers: 3, category: 1)
     end
   end
 end
